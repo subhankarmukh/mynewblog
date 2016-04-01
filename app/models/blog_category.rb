e76af4d64c7f3a2bd98256899a771018b3566f06
Subhankar Mukherjee <skrmuk2700c@gmail.com>
2016-04-01 23:08:14 +0530

    ADDED: Data validation added

--- app/models/blog_category.rb
+++ app/models/blog_category.rb
@@ -1,7 +1,17 @@
 class BlogCategory < ActiveRecord::Base
+	#Association
 	has_and_belongs_to_many :blog_posts,
 										#class_name: BlogPost,
 										join_table: :categories_posts,
 										foreign_key: :category_id,
 										association_foreign_key: :post_id
+    #Validation
+	SLUG_REGEX = /\A^[\w&-]+$\Z/
+	validates :title,   presence: true,
+	                       		length: { maximum: 250 }
+	validates :slug,    presence: true,
+		                        uniqueness: { :case_sensitive => false },
+		                        length:     { maximum: 250 },
+		                        format:  	 SLUG_REGEX
+	validates :content, presence: true, length: { maximum: 500 }									
 end
--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,4 +1,6 @@
 class BlogPost < ActiveRecord::Base
+
+	#associations
 	has_and_belongs_to_many :blog_categories, 
 											  #class_name: BlogCategory,
 											  join_table: :categories_posts,
@@ -6,4 +8,10 @@ class BlogPost < ActiveRecord::Base
 											  association_foreign_key: :category_id
 
     belongs_to :User
+
+    #validation
+    validates :title, presence: true,
+                      length: { maximum: 250 }		
+    validates :content, presence: true, 
+    		    		length: { minimum: 10 }               
 end
