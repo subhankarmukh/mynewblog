505ba315d901ddbb0503243ae69acb024b712577
Subhankar Mukherjee <skrmuk2700c@gmail.com>
2016-04-01 20:16:48 +0530

    ADDED: join table blog_categories_posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160401125717) do
+ActiveRecord::Schema.define(version: 20160401144419) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -62,6 +62,14 @@ ActiveRecord::Schema.define(version: 20160401125717) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "categories_posts", id: false, force: :cascade do |t|
+    t.integer "category_id", null: false
+    t.integer "post_id",     null: false
+  end
+
+  add_index "categories_posts", ["category_id", "post_id"], name: "index_categories_posts_on_category_id_and_post_id"
+  add_index "categories_posts", ["post_id", "category_id"], name: "index_categories_posts_on_post_id_and_category_id"
 
   create_table "roles", force: :cascade do |t|
     t.string   "name"
