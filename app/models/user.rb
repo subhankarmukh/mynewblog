7b1cf0a61507c0e4bd70af7a9736b983d0966df4
Subhankar Mukherjee <skrmuk2700c@gmail.com>
2016-04-01 16:12:08 +0530

    Registered: activeadmin, Hotfix: rolify

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ActiveRecord::Base
   rolify
+
+  # Active Record Callbacks
+  after_create :add_user_role_in_registration
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  private
+  	def add_user_role_in_registration
+  		self.add_role 'user'
+  	end       
 end
